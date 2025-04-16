97246e8509afaee8e0e222852372b804a93abc79
Slavalyub <v.lyub4enko@mail.ru>
2025-04-16 10:52:23 +0300

    добавляю alembic в проект

--- app/core/config.py
+++ app/core/config.py
@@ -1,9 +1,20 @@
-# app/core/config.py
-from pydantic import BaseSettings
+# core/config.py
+from pydantic import BaseSettings, Field
+from datetime import date
 
 
 class Settings(BaseSettings):
-    ...
+    app_title: str = Field(..., env='APP_TITLE')
+    app_author: str = Field(..., env='APP_AUTHOR')
+    author_pass: str = Field(..., env='AUTHOR_PASS')
+    deadline_date: date = Field(..., env='DEADLINE_DATE')
+    # path: Path = Field(..., env='PATH')
+
+    database_url: str
 
     class Config:
         env_file = '.env'
+        # env_prefix = 'MYAPP_'  # используем префикс по умолчанию, можно не задавать, если в Field указываешь env
+
+
+settings = Settings()
--- app/main.py
+++ app/main.py
@@ -2,6 +2,8 @@ from fastapi import FastAPI
 
 from app.api.endpoints import router
 
-app = FastAPI()
+from app.core.config import settings
+
+app = FastAPI(title=settings.app_title, description=settings.app_author)
 
 app.include_router(router)

b3c6438926e322eca085c8eb061b5bf0c9542bf4
Slavalyub <v.lyub4enko@mail.ru>
2025-04-18 15:57:29 +0300

    добавил связи таблицы user с reservation

--- app/api/endpoints/user.py
+++ app/api/endpoints/user.py
@@ -1,10 +1,12 @@
 # app/api/endpoints/user.py
+
 from fastapi import APIRouter, Depends
 
+from sqlalchemy import select
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from app.core.db import get_async_session
-from app.core.user import auth_backend, fastapi_users
+from app.core.user import auth_backend, fastapi_users, current_user
 from app.models import User
 from app.schemas.user import UserCreate, UserRead, UserUpdate
 
@@ -22,9 +24,24 @@ router.include_router(
     prefix='/auth',
     tags=['auth'],
 )
+
+users_router = fastapi_users.get_users_router(UserRead, UserUpdate)
+# Из списка эндпоинтов роутера исключаем ненужную ручку.
+users_router.routes = [
+    rout for rout in users_router.routes if rout.name != 'users:delete_user'
+]
+
 router.include_router(
-    fastapi_users.get_users_router(UserRead, UserUpdate),
+    users_router,
     prefix='/users',
     tags=['users'],
 )
 
+
+@router.get("/all-users/", response_model=list[UserRead], tags=["users"])
+async def get_all_users(
+    session: AsyncSession = Depends(get_async_session),
+):
+    result = await session.execute(select(User))
+    users = result.scalars().all()
+    return users
